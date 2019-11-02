ed189aa3e3069f95a4910c755d6b2d4e22601c01
spawulski <spawulski@gmail.com>
2019-11-02 13:53:28 -0500

    Did a bunch of work

--- final-project/scrape_weather.py
+++ final-project/scrape_weather.py
@@ -1,21 +1,40 @@
 from html.parser import HTMLParser
 from html.entities import name2codepoint
 import urllib.request
+import datetime
 
 
 class MyHTMLParser(HTMLParser):
     attr_flag = False
     data_counter = 0
+    weather = {}
+    date = ''
+    day = ''
+    min = ''
+    max = ''
+    mean = ''
+
     bad_words = ['scheme', 'W3CDTF', 'Google Tag Manager',
                  """Environment and Climate Change Canada -
                   Meteorological Service of Canada""", 'degrees', 'minute',
                  'second', 'North', 'West', 'East', 'South',
                  'World Meteorological Organization',
                  'Transport Canada', 'Comma Separated Values',
-                 'Extensible Markup Language', 'metre']
-
-
-                 #FINISH THE BAD WORDS ARRAY YOU LAZY BASTARD.
+                 'Extensible Markup Language', 'metre', 'Maximum Temperature',
+                 'degrees Celsius', 'Daily Maximum Temperature Chart',
+                 'Minimum Temperature', 'Daily Minimum Temperature Chart',
+                 'Mean Temperature', 'Daily Mean Temperature Chart',
+                 'Heating Degree Days', 'Daily Heating Degree Days Chart',
+                 'millimetres', 'Daily Total Rain Chart', 'centimetres',
+                 'Daily Total Snow Chart', 'Total Precipitation',
+                 'Daily Total Precipitation Chart', 'Cooling Degree Days',
+                 'Daily Cooling Degree Days Chart', 'Snow on Ground',
+                 'Daily Snow on Ground Chart', 'Direction of Maximum Gust',
+                 'tens of degrees', 'Speed of Maximum Gust',
+                 'kilometres per hour', 'Daily Speed of Maximum Gust Chart',
+                 'Environment and Climate Change Canada - Meteorological Service of Canada',
+                 'Average', 'Extreme'
+                 ]
 
 
 
@@ -24,10 +43,12 @@ class MyHTMLParser(HTMLParser):
         for attr in attrs:
             if "title" in attr:
                 if attr[1] not in self.bad_words:
+                    #self.date = attr[1]
+                    self.date = datetime.datetime.strptime(attr[1], '%B %d, %Y').strftime('%Y-%m-%d')
                     self.attr_flag = True
                     #date_array = attr[1].split(" ")
                     #date_array[1] = date_array[1][:-1]
-                    print("     attr:", attr)
+                    #print("     attr:", attr)
 
 
 
@@ -40,6 +61,13 @@ class MyHTMLParser(HTMLParser):
             return False
 
 
+    def data_reset(self):
+        self.day = ''
+        self.max = ''
+        self.min = ''
+        self.mean = ''
+
+
     #def handle_endtag(self, tag):
     #    print("End tag :", tag)
 
@@ -47,12 +75,25 @@ class MyHTMLParser(HTMLParser):
         if self.attr_flag:
             if self.float_checker(data.strip()):
                 if self.data_counter < 4:
-                    print("Data  :", data)
+                    #print("Data  :", data)
+                    if self.data_counter == 0:
+                        self.day = data
+                    if self.data_counter == 1:
+                        self.max = data
+                    if self.data_counter == 2:
+                        self.min = data
+                    if self.data_counter == 3:
+                        self.mean = data
+
                     #print(self.data_counter)
                     self.data_counter += 1
                 else:
+                    daily_temps = {'Max': self.max, 'Min': self.min, 'Mean': self.mean}
+                    self.weather = {self.date: daily_temps}
                     self.attr_flag = False
                     self.data_counter = 0
+                    self.data_reset()
+
 
     #def handle_comment(self, data):
     #    print("Comment  :", data)
@@ -72,9 +113,39 @@ class MyHTMLParser(HTMLParser):
 #        print("Decl     :", data)
 
 
+#url = "https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2018&Month=5"
+
+years = ['1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003',
+         '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011',
+         '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019']
+
+#months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+
+
 myparser = MyHTMLParser()
 
-with urllib.request.urlopen('https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2018&Month=5') as response:
-    html = str(response.read())
+weather = {}
+all_weather = {}
 
-myparser.feed(html)
+def get_data(url):
+    with urllib.request.urlopen(url) as response:
+        html = str(response.read())
+
+    myparser.feed(html)
+
+    for k, v in myparser.weather.items():
+        weather.update({k: v})
+
+    return weather
+
+
+for year in range(2019, 1995, -1):
+    print(str(year))
+    for month in range(12, 0, -1):
+        print(str(month))
+        url = 'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=' + str(year) + '&Month=' + str(month)
+        w = get_data(url)
+        for k, v in w.items():
+            all_weather.update({k: v})
+
+print(all_weather)
