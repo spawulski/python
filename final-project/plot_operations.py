113beab9459fc6df5184e58919d27fdc0dab8338
spawulski <spawulski@gmail.com>
2019-11-09 13:04:50 -0600

    Doc string + commented build plot code

--- final-project/plot_operations.py
+++ final-project/plot_operations.py
@@ -19,37 +19,48 @@ class PlotOperations():
 
     def build_plot(self, a, b):
         """Build a plot using data that is passed in."""
+        # Instantiate db object
         db = db_operations.DBOperations()
+        # Query db for all weather data from year a - year b
         data = db.between(str(a), str(b))
 
+        # Iterate through every line of data returned from db Query
         for item in data:
+            # Get number of month from db entry
             month = int(item[0][5:7])
+            # If a single digit month, trim off leading 0
             if month < 10:
                 month = int(item[0][6:7])
 
             mean = item[3]
 
+            # If month from db entry matches with month_dict key
             for key in self.months_dict.keys():
                 if key == month:
+                    # There were a few outliers/bad data that this filters out
                     if mean < 100:
+                        # add temperature data for that day to month_dict value
                         self.months_dict[key].append(mean)
 
+        # Iterate through month values
         for month in range(1, 13, 1):
             try:
+                # Create an array of arrays for boxplot
                 self.months_array.append(self.months_dict[month])
             except Exception as e:
                 print("Error: ", e)
 
+        # Set params for boxplot along with data to populate
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.boxplot(self.months_array)
         mtd = "Monthy Temperature Distrubution for: "
         ax.set_title(mtd + str(a) + " to " + str(b))
         ax.set_xlabel("Month")
-        ax.set_ylabel("Temperature (Celcius")
+        ax.set_ylabel("Temperature (Celcius)")
 
         plt.show()
 
-
-plot = PlotOperations()
-plot.build_plot(1996, 2019)
+# Build Plot example
+# plot = PlotOperations()
+# plot.build_plot(1996, 2019)
